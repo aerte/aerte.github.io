dcf6372b6e28dbfc3c0792eac017ca27f60f0485
aerte <aertebjerg.felix@gmail.com>
2026-01-25 13:44:19 +0100

    Add .nojekyll to build output for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
   images: {
     unoptimized: true, // Required for static export
   },
+  // Ensure .nojekyll is copied to output
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.afterEmit.tap('CopyNojekyll', () => {
+            const fs = require('fs');
+            const path = require('path');
+            const source = path.join(process.cwd(), '.nojekyll');
+            const dest = path.join(process.cwd(), 'out', '.nojekyll');
+            if (fs.existsSync(source)) {
+              fs.copyFileSync(source, dest);
+            }
+          });
+        },
+      });
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
